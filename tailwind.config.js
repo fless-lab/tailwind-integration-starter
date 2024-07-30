a59ede7e4297712376f29607f915036b368e722f
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-07-30 19:38:35 +0000

    setup completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0DE1F2',
+        secondary: '#14171A',
+      },
+    },
+  },
+  plugins: [],
+};
